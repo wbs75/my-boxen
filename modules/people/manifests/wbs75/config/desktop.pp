b688cb55da069de18d234514634e0600a01ede00
wbs75 <wbs75@me.com>
2014-07-06 00:21:26 -0600

    local change

--- modules/people/manifests/wbs75/config/desktop.pp
+++ modules/people/manifests/wbs75/config/desktop.pp
@@ -13,8 +13,9 @@ class people::wbs75::config::desktop {
     }
 
     exec { 'Set Desktop':
-        user        => 'root',
-        command     =>  '/Library/Ruby/Gems/2.0.0/gems/desktop-1.1.1/bin/desktop http://fc05.deviantart.net/fs71/f/2012/282/6/c/apple_dark_by_mikesgame-d5hb6i1.jpg',
+        user        =>  'root',
+        command     =>  'desktop http://fc05.deviantart.net/fs71/f/2012/282/6/c/apple_dark_by_mikesgame-d5hb6i1.jpg',
+        path        =>  "/Library/Ruby/Gems/2.0.0/gems/desktop-1.1.1/bin/",
         require     =>  Exec['Ruby Install Desktop']
     }
 }
