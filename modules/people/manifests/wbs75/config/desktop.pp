ebadc3fc9c5f2e1e355122cc41a10af7df551d2e
wbs75 <wbs75@me.com>
2014-07-06 00:02:19 -0600

    local change

--- modules/people/manifests/wbs75/config/desktop.pp
+++ modules/people/manifests/wbs75/config/desktop.pp
@@ -6,6 +6,12 @@ class people::wbs75::config::desktop {
         mode    =>'0755',
     }
 
+    exec { 'Ruby Install Desktop':
+        user        => 'root',
+        command     => '/usr/bin/sudo /usr/bin/gem install desktop',
+        creates     => ["/Library/Ruby/Gems/2.0.0/gems/desktop-1.1.1/bin/desktop"],
+    }
+
     exec { 'Set Desktop':
         user        => 'root',
         command     =>  '/Library/Ruby/Gems/2.0.0/gems/desktop-1.1.1/bin/desktop http://fc05.deviantart.net/fs71/f/2012/282/6/c/apple_dark_by_mikesgame-d5hb6i1.jpg',
