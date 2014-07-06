8ac4f02e819ab2de1ab18bed0ef6a2066bf440b8
wbs75 <wbs75@me.com>
2014-07-06 01:12:14 -0600

    local change

--- manifests/site.pp
+++ manifests/site.pp
@@ -34,17 +34,17 @@ Package {
   install_options => ['--build-from-source'],
 }
 
-Repository {
-  provider => git,
-  extra    => [
-    '--recurse-submodules'
-  ],
-  require  => File["${boxen::config::bindir}/boxen-git-credential"],
-  config   => {
-    'credential.helper' => "${boxen::config::bindir}/boxen-git-credential"
-  }
-
-}
+# Repository {
+#   provider => git,
+#   extra    => [
+#     '--recurse-submodules'
+#   ],
+#   require  => File["${boxen::config::bindir}/boxen-git-credential"],
+#   config   => {
+#     'credential.helper' => "${boxen::config::bindir}/boxen-git-credential"
+#   }
+#
+# }
 
 Service {
   provider => ghlaunchd
