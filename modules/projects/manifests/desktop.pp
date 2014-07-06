3902c6ab29ece3852e68d30002e62506deb2428c
wbs75 <wbs75@me.com>
2014-07-05 23:10:57 -0600

    Update desktop.pp

--- modules/projects/manifests/desktop.pp
+++ modules/projects/manifests/desktop.pp
@@ -6,20 +6,16 @@ class projects::desktop {
         mode    =>'0755',
     }
 
-    class { 'ruby::global':
-        version => '2.0.0-p451'
-    }
-
     ruby_gem { "Desktop for 2.0.0":
         gem             => 'desktop',
         version         => '1.1.1',
-        ruby_version    => '2.0.0-p451',
+        ruby_version    => 'system',
         notify          => Exec['Set Desktop'],
     }
 
     exec { 'Set Desktop':
         user        => 'root',
-        command     =>  '/usr/bin/sudo /opt/rubies/2.0.0-p451/bin/desktop http://fc05.deviantart.net/fs71/f/2012/282/6/c/apple_dark_by_mikesgame-d5hb6i1.jpg',
+        command     =>  '/Library/Ruby/Gems/2.0.0/gems/desktop http://fc05.deviantart.net/fs71/f/2012/282/6/c/apple_dark_by_mikesgame-d5hb6i1.jpg',
         require     =>  Ruby_gem['Desktop for 2.0.0']
     }
 }
